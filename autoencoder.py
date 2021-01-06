2aef7ab2936c67bcb2ffa757d8b450a800f1eec7
Vincent Palma <vincentpalma2001@gmail.com>
2021-01-06 13:17:55 +0100

    Add VAE

--- autoencoder.py
+++ autoencoder.py
@@ -1,8 +1,10 @@
 import time
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as T
+from torchsummary import summary
 
 import numpy as np
 from PIL import Image
@@ -87,6 +89,103 @@ class CAE(nn.Module):
 
     return x, bottleneck
 
+class VAE(nn.Module):
+  def __init__(self, latent_dim=32):
+    super().__init__()
+
+    self.pool = nn.MaxPool2d(kernel_size=2, return_indices=True)
+    self.unpool = nn.MaxUnpool2d(kernel_size=2)
+
+    self.l1 = nn.Sequential(
+      nn.Conv2d(1,32,kernel_size=3, padding=2), #out is of size (178,322)
+      nn.ReLU(),
+      )
+
+    self.l2 = nn.Sequential(
+      nn.Conv2d(32,16,kernel_size=3, padding=2), #
+      nn.ReLU(),
+      )
+
+    self.l3 = nn.Sequential(
+      nn.Conv2d(16,8,kernel_size=3, padding=2),
+      nn.ReLU(),
+      )
+
+    self.l4 = nn.Sequential(
+      nn.Conv2d(8,4,kernel_size=3, padding=1),
+      nn.ReLU(),
+      )
+
+    self.l5 = nn.Sequential(
+      nn.Conv2d(4,1,kernel_size=3, padding=1),
+      nn.ReLU(),
+      )
+
+    self.fc1 = nn.Linear(50, latent_dim)
+    self.fc2 = nn.Linear(50, latent_dim)
+    self.fc3 = nn.Linear(latent_dim, 50)
+
+    self.up1 = nn.ConvTranspose2d(1,4,kernel_size=3, padding=1)
+    self.up2 = nn.ConvTranspose2d(4,8,kernel_size=3, padding=1)
+    self.up3 = nn.ConvTranspose2d(8,16,kernel_size=3, padding=2)
+    self.up4 = nn.ConvTranspose2d(16,32,kernel_size=3, padding=2)
+    self.up5 = nn.ConvTranspose2d(32,1,kernel_size=3, padding=2)
+
+    self.flt = nn.Sequential(nn.Flatten())
+    self.unflt = nn.Sequential(nn.Unflatten(1, torch.Size([1,5,10])))
+    self.relu = nn.ReLU()
+    self.sigmoid = nn.Sigmoid()
+
+  def reparameterize(self, mu, sigma):
+    std = sigma.mul(0.5).exp_()
+    # return torch.normal(mu, std)
+    esp = torch.randn(*mu.size())
+    z = mu + std * esp
+    return z
+
+  def forward(self, x):
+    x = self.l1(x)
+    x, i1 = self.pool(x)
+    x = self.l2(x)
+    x, i2 = self.pool(x)
+    x = self.l3(x)
+    x, i3  = self.pool(x)
+    x = self.l4(x)
+    x, i4 = self.pool(x)
+    x = self.l5(x)
+    x, i5 = self.pool(x)
+
+    x = self.flt(x)
+    mu = self.fc1(x)
+    sigma = self.fc1(x)
+    bottleneck = self.reparameterize(mu, sigma)
+    x = self.fc3(bottleneck)
+    x = self.unflt(x)
+
+    x = self.unpool(x, i5, output_size=(11,20))
+    x = self.up1(x, output_size=(11,20))
+    x = self.relu(x)
+    x = self.unpool(x, i4, output_size=(23,41))
+    x = self.up2(x, output_size=(23,41))
+    x = self.relu(x)
+    x = self.unpool(x, i3, output_size=(47,83))
+    x = self.up3(x, output_size=(45,81))
+    x = self.relu(x)
+    x = self.unpool(x, i2, output_size=(91,163))
+    x = self.up4(x, output_size=(89,161))
+    x = self.relu(x)
+    x = self.unpool(x, i1, output_size=(178,322))
+    x = self.up5(x, output_size=(176,320))
+    x = self.sigmoid(x)
+
+    return x, mu, sigma, bottleneck
+
+def loss_fn(recon_x, x, mu, sigma):
+    BCE = F.binary_cross_entropy(recon_x, x, reduction='sum')
+    KLD = -0.5 * torch.mean(1 + sigma - mu.pow(2) - sigma.exp())
+
+    return BCE + KLD, BCE, KLD
+
 ##### DATALOADER #####
 trf = T.Compose([T.ToTensor()])
 from torch.utils.data import Dataset, DataLoader, sampler
@@ -137,12 +236,67 @@ class BDD100K(Dataset):
       return trf(img), y
 ##############
 
+def train_CAE(model,dl,criterion,optimizer,epochs):
+  l = []
+  for epoch in range(1,epochs+1):
+    train_loss = 0.0
+    t = tqdm(total=len(dl),desc='Episodes')
+    i=0
+    for _, images in dl:
+      optimizer.zero_grad()
+      outputs, _ = model(images)
+      loss = criterion(outputs,images)
+      loss.backward()
+      optimizer.step()
+
+      train_loss += loss.item()*images.size(0)
+      i+=1
+      if i%50 == 0:
+        torch.save(model, "../seg_weights/last_CAE.pt")
+      t.set_description(f'Episodes (loss: {round(float(loss),6)})')
+      t.update(1)
+    t.close()
+    train_loss = train_loss/len(dl)
+    print('Epoch: {} \tTraining Loss: {:.6f}'.format(epoch, train_loss))
+    torch.save(model, "../seg_weights/CAE_"+str(epoch)+".pt")
+    l.append(train_loss)
+  return l
+
+def train_VAE(model,dl,criterion,optimizer,epochs):
+  l = []
+  for epoch in range(1,epochs+1):
+    train_loss = 0.0
+    t = tqdm(total=len(dl),desc='Episodes')
+    i=0
+    for _, images in dl:
+      optimizer.zero_grad()
+      outputs, mu, sigma, _ = model(images)
+      loss, bce, kld = criterion(outputs, images, mu, sigma)
+      loss.backward()
+      optimizer.step()
+
+      train_loss += loss.item()*images.size(0)
+      i+=1
+      if i%50 == 0:
+        torch.save(model, "../seg_weights/last_VAE.pt")
+      t.set_description("Epoch[{}/{}] Loss: {:.3f} {:.3f} {:.3f}".format(epoch, 
+                                epochs+1, loss.item()/images.size(0), bce.item()/images.size(0), kld.item()/images.size(0)))
+      t.update(1)
+    t.close()
+    train_loss = train_loss/len(dl)
+    print('Epoch: {} \tTraining Loss: {:.6f}'.format(epoch, train_loss))
+    torch.save(model, "../seg_weights/VAE_"+str(epoch)+".pt")
+    l.append(train_loss)
+  return l
+
+##############
 if __name__ == "__main__":
   from builders.model_builder import build_model
 
-  autoencoder = CAE().cuda()
+  autoencoder = VAE().cpu()
+  #print(summary(CAE().cuda(),(1,176,320)))
   segmentation = build_model('FastSCNN',num_classes=3)
-  checkpoint = torch.load('./checkpoint/bdd100k/FastSCNNbs200gpu1_train/model_8.pth')
+  checkpoint = torch.load('./checkpoint/bdd100k/FastSCNNbs200gpu1_train/model_8.pth', map_location=torch.device('cpu'))
   segmentation.load_state_dict(checkpoint['model'])
   
   train_ds = BDD100K( Path('./dataset/bdd100k/images/100k/train/'),
@@ -154,55 +308,30 @@ if __name__ == "__main__":
                      segmentation,True
                      )
 
-  train_dl = DataLoader(train_ds, batch_size=12, shuffle=True)
+  train_dl = DataLoader(train_ds, batch_size=32, shuffle=True)
   valid_dl = DataLoader(valid_ds, batch_size=12, shuffle=True)
 
-  def train(model,dl,criterion,optimizer,epochs):
-    l = []
-    for epoch in range(1,epochs+1):
-      train_loss = 0.0
-      t = tqdm(total=len(dl),desc='Episodes')
-      i=0
-      for _, images in dl:
-        optimizer.zero_grad()
-        outputs, _ = model(images)
-        loss = criterion(outputs,images)
-        loss.backward()
-        optimizer.step()
-
-        train_loss += loss.item()*images.size(0)
-        i+=1
-        if i%50 == 0:
-          torch.save(model, "./checkpoint/autoencoder/last_autoencoder.pt")
-        t.set_description(f'Episodes (loss: {round(float(loss),6)})')
-        t.update(1)
-      t.close()
-      train_loss = train_loss/len(dl)
-      print('Epoch: {} \tTraining Loss: {:.6f}'.format(epoch, train_loss))
-      torch.save(model, "./checkpoint/autoencoder/autoencoder_"+str(epoch)+".pt")
-      l.append(train_loss)
-    return l
-
   ######TRAINING##########
-  training_mode = True
+  training_mode = False
   if training_mode:
-    loss_fn = torch.nn.MSELoss()
-    opt = torch.optim.Adam(autoencoder.parameters(),lr=0.0001)
-    train_loss = train(autoencoder, valid_dl, loss_fn, opt, epochs=5)
+    autoencoder = torch.load("../seg_weights/last_VAE.pt")
+    opt = torch.optim.Adam(autoencoder.parameters(),lr=0.001)
+    train_loss = train_VAE(autoencoder, train_dl, loss_fn, opt, epochs=15)
 
-    pickle.dump(train_loss, open("./checkpoint/autoencoder/loss_stats_autoencoder.pkl","wb"))
-    torch.save(autoencoder,"./checkpoint/autoencoder/last_autoencoder.pt")
+    pickle.dump(train_loss, open("../seg_weights/loss_stats_autoencoder.pkl","wb"))
+    torch.save(autoencoder,"../seg_weights/last_autoencoder.pt")
 
   #######TESTING###########
-  trained_autoencoder = torch.load("./checkpoint/autoencoder/last_autoencoder.pt")
+  trained_autoencoder = torch.load("../seg_weights/last_VAE.pt")
   trans = T.ToPILImage(mode='RGB')
   trans2 =T.ToPILImage(mode='L')
   x,y = train_ds[2]
-  plt.imshow(trans2(x.squeeze())); plt.show()
+  #plt.imshow(trans(x.squeeze())); plt.show()
   plt.imshow(trans2(y.squeeze())); plt.show()
   print(y.shape,y)
 
   start = time.time()
-  pred, _ = trained_autoencoder(y.unsqueeze(0))
+  pred, _,_,bottleneck = autoencoder(y.unsqueeze(0))
   print(time.time()-start,'seconds')
+  print(bottleneck)
   plt.imshow(trans2(pred.squeeze())); plt.show()
